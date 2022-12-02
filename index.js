e40c8cbcd302328eff0865e4733593d445c9ed40
Vusalhey <vusal.tot2017@yandex.ru>
2022-12-02 03:28:40 +0300

    eslint redact

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-console.log("Hello, Hexlet!");
-import _ from "lodash";
+/* eslint-disable no-console */
+import _ from 'lodash';
+
+console.log('Hello, Hexlet!');
 _.intersection([2, 1], [2, 3]);
-_.capitalize("hello");
-console.log(_.last(["one", "two"]));
+_.capitalize('hello');
+console.log(_.last(['one', 'two']));
